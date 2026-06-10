cc76511d97355e5d7fdd4c7866a596bb2b63e3e7
mercyed <alexisjbarraza10@gmail.com>
2026-06-10 13:25:17 -0400

    progress so far

--- labs/lab07_operator_overloading_comparisons/starter_code/time_duration.py
+++ labs/lab07_operator_overloading_comparisons/starter_code/time_duration.py
@@ -29,13 +29,19 @@ class TimeDuration:
         """
         Return True if two TimeDuration objects represent the same duration.
         """
-        return self.total_seconds() == other.total_seconds()
+        if self.total_seconds() == other.total_seconds():
+            return True
+        else:
+            return False
 
     def __lt__(self, other):
         """
         Return True if this duration is shorter than the other duration.
         """
-        return self.total_seconds() < other.total_seconds()
+        if self.total_seconds() < other.total_seconds():
+            return True
+        else:
+            return False
 
     def __add__(self, other):
         """
