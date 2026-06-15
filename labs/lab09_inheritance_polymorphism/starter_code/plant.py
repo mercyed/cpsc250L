b93d88307ea5925e39a85d900e71d74f03d6062c
mercyed <alexisjbarraza10@gmail.com>
2026-06-15 13:18:43 -0400

    progress so far

--- labs/lab09_inheritance_polymorphism/starter_code/plant.py
+++ labs/lab09_inheritance_polymorphism/starter_code/plant.py
@@ -1,31 +1,34 @@
 class Plant:
-    def __init__(self, name, height_cm):
-        pass
+    def __init__(self, name, height_cm=20):
+        self.name = name
+        self.height_cm = height_cm
 
     def care_instructions(self):
-        pass
+        return f"Water regularly and provide adequate sunlight."
 
     def __str__(self):
-        pass
+        return f"Plant: {self.name}, Height: {self.height_cm}cm"
 
 
 class Flower(Plant):
-    def __init__(self, name, height_cm, color):
-        pass
+    def __init__(self, name, height_cm=30, color="Green"):
+        super().__init__(name,height_cm)
+        self.color = color
 
     def care_instructions(self):
-        pass
+        return "Water regularly, provide full sun, and deadhead spent blooms."
 
     def __str__(self):
-        pass
+        return f"Flower: {self.name}, Height: {self.height_cm} cm, Color: {self.color}"
 
 
 class Vegetable(Plant):
-    def __init__(self, name, height_cm, harvest_days):
-        pass
+    def __init__(self, name, height_cm=10, harvest_days=90):
+        super().__init__(name,height_cm)
+        self.harvest = harvest_days
 
     def care_instructions(self):
-        pass
+        return f"Water regularly, provide full sun, and fertilize every two weeks."
 
     def __str__(self):
-        pass
+        return f"Vegetable: {self.name}, Height: {self.height_cm}cm, Harvest Days: {self.harvest}"
