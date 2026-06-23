423f214265c9b7a7f4e7c59f1cac095b232d60c4
mercyed <alexisjbarraza10@gmail.com>
2026-06-23 09:17:08 -0400

    progress so far

--- labs/lab12_numpy_matplotlib_arrays/starter_code/motion_plot.py
+++ labs/lab12_numpy_matplotlib_arrays/starter_code/motion_plot.py
@@ -2,10 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def position(t, x0, v0, a):
-    pass
+    return  x0 + v0 * t + 0.5 * a * t**2
+
 
 def velocity(t, v0, a):
-    pass
+    return v0 + a * t
 
 def main():
     # Consider a projectile launched at a speed of 50 m/s and an angle of 45 degrees.
