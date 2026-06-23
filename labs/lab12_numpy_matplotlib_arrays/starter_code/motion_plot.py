7de6d482555df96463d275ad389e8320c2b93297
mercyed <alexisjbarraza10@gmail.com>
2026-06-23 09:32:04 -0400

    progress so far

--- labs/lab12_numpy_matplotlib_arrays/starter_code/motion_plot.py
+++ labs/lab12_numpy_matplotlib_arrays/starter_code/motion_plot.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def position(t, x0, v0, a):
-    return  x0 + v0 * t + 0.5 * a * t**2
+    return x0 + v0 * t + 0.5 * a * t**2
 
 
 def velocity(t, v0, a):
@@ -17,7 +17,53 @@ def main():
     # TODO: create time array using np.linspace
     # TODO: compute position and velocity arrays
     # TODO: make and save plots
-    pass
+    speed = 50
+    angle = np.radians(45)
+
+    vx0 = speed * np.cos(angle)
+    vy0 = speed * np.sin(angle)
+
+    ax = 0
+    ay = -9.8
+
+    t = np.linspace(0, 10, 100)
+
+    x = position(t, 0, vx0, ax)
+    y = position(t, 0, vy0, ay)
+
+    vx = velocity(t, vx0, ax)
+    vy = velocity(t, vy0, ay)
+
+    plt.figure()
+    plt.plot(t, x)
+    plt.xlabel("Time (s)")
+    plt.ylabel("x position (m)")
+    plt.title("x vs. t")
+    plt.savefig("x_vs_t.png")
+
+    plt.figure()
+    plt.plot(t, y)
+    plt.xlabel("Time (s)")
+    plt.ylabel("y position (m)")
+    plt.title("y vs. t")
+    plt.savefig("y_vs_t.png")
+
+    plt.figure()
+    plt.plot(t, vx)
+    plt.xlabel("Time (s)")
+    plt.ylabel("x velocity (m/s)")
+    plt.title("vx vs. t")
+    plt.savefig("vx_vs_t.png")
+
+    plt.figure()
+    plt.plot(t, vy)
+    plt.xlabel("Time (s)")
+    plt.ylabel("y velocity (m/s)")
+    plt.title("vy vs. t")
+    plt.savefig("vy_vs_t.png")
+
+    plt.show()
+
 
 
 main()
